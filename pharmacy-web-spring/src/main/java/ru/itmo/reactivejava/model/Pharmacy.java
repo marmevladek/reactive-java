2e0af11bace28a579287c4006d8b557319001189
Chubru <chubru.alexander@gmail.com>
2025-01-08 01:43:52 +0300

    Fix typo in 'pharmacy' table name
    Signed-off-by: Chubru <chubru.alexander@gmail.com>

--- pharmacy-web-spring/src/main/java/ru/itmo/reactivejava/model/Pharmacy.java
+++ pharmacy-web-spring/src/main/java/ru/itmo/reactivejava/model/Pharmacy.java
@@ -5,7 +5,7 @@ import lombok.*;
 import org.springframework.data.relational.core.mapping.Column;
 import org.springframework.data.relational.core.mapping.Table;
 
-@Table("pharmacies")
+@Table("pharmacy")
 @Getter
 @Setter
 @NoArgsConstructor
